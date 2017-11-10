4c6d31afd9740b854841bbc16fa4fad3953ce829
Patrick Ecord <pecord@gmail.com>
2017-11-10 10:16:04 -0600

    Add balance check

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ if (!process.env.COINBASE_API_SECRET) {
   throw new Error("Coinbase API key missing")
 }
 
+if (!process.env.COINBASE_STOP_LOSS) {
+  throw new Error("Coinbase stop loss price is missing")
+}
+
 let stopLossPrice = safelyParseNumber(process.env.COINBASE_STOP_LOSS)
 if(stopLossPrice <= 0) {
   throw new Error("Stop loss price must be greater than zero")
@@ -24,36 +28,41 @@ var client = new Client({
 
 async.doWhilst((done) => {
   debug("*********************************************************************")
-  getSellPrice((err, sellPrice) => {
-    if (err) {
-      debug("get sell price returned with an error", err);
-      return done(err)
-    } else {
-      if (!sellPrice) {
-        return done(new Error("no sale price"))
-      }
+  async.series({
+    btcTotal: (cb) => getBitcoinTotal(cb),
+    sellPrice: (cb) => getSellPrice(cb)
+  }, (err, results) => {
+    if (err) return done(err)
+    let {btcTotal, sellPrice} = results
 
-      debug("got sell price", sellPrice);
-      let isItTimeToSell = sellPrice < stopLossPrice
-      debug(`is it time to sell... ${isItTimeToSell ? "YES!" : "no"} ${sellPrice} < ${stopLossPrice}`)
+    if (!sellPrice) return done(new Error("no sale price"))
+    debug("got sell price", sellPrice);
 
-      if (isItTimeToSell) {
-        debug("selling EVERYTHING!!!... you are welcome :)")
+    if (!btcTotal) return done(new Error("no btc total"))
+    debug("got btc total", btcTotal)
+
+    let isItTimeToSell = sellPrice < stopLossPrice
+
+    debug(`is it time to sell... ${isItTimeToSell ? "YES!" : "no"} ${sellPrice} < ${stopLossPrice}`)
+    if (isItTimeToSell) {
+      debug("selling EVERYTHING!!!... you are welcome :)")
+      sellBitcoin(btcTotal, (err, xfer) => {
+        debug("sell complete", xfer)
         process.exit(0);
-      } else {
-        debug("it is not time to sell")
-        return done(null)
-      }
+      })
+    } else {
+      debug("it is not time to sell")
+      return done(null)
     }
   })
 }, () => { return true })
 
 function safelyParseNumber(numberToBeParsed) {
-  if (!numberToBeParsed) {
-    throw new Error("Number to be parsed is missing")
-  } else {
+  try {
     let parsedNumber = parseFloat(numberToBeParsed, 10)
     return parsedNumber
+  } catch(err) {
+    throw err
   }
 }
 
@@ -72,5 +81,27 @@ function getSellPrice(cb) {
         return cb(null, sellPrice)
       }
     }
+  });
+}
+
+function getBitcoinTotal(cb) {
+  client.getAccounts({}, function(err, accounts) {
+    if(err) return cb(err)
+    accounts.forEach(function(acct) {
+      if(acct.balance.currency === "BTC")
+      return cb(null, acct.balance.amount)
+    })
+  })
+}
+
+function sellBitcoin(amount, cb) {
+  var args = {
+    "amount": amount,
+    "currency": "BTC"
+  };
+  account.sell(args, function(err, xfer) {
+    if(err) return cb(err)
+    console.log('my xfer id is: ' + xfer.id);
+    return cb(null, xfer)
   });
 }
